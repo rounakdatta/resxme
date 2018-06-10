aba0bcd12b4874374d49ad6b03b46a5a81519f4c
SauravCR7 <sauravsaha48@gmail.com>
2018-06-10 22:52:08 +0530

    handling docx files

--- app.py
+++ app.py
@@ -11,88 +11,95 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/preprocess', methods=['GET', 'POST'])
 def preprocess():
 
-	f = open('./data/pstatus', 'r')
-	processed = f.read()
-	f.close()
+    f = open('./data/pstatus', 'r')
+    processed = f.read()
+    f.close()
 
-	n = int(processed)
-	total = len(os.listdir('./data/resume_dataset/'))
+    n = int(processed)
+    total = len(os.listdir('./data/resume_dataset/'))
 
-	for i in range(n + 1, total + 1):
-		validator.resume_score(str(i))
-		print(i)
+    dirs=os.listdir('./data/resume_dataset/')
+    print(dirs)
+    for i in range(n + 1, total + 1):
+        if(dirs[i-1].find("pdf")>0):
+            print(dirs[i-1])
+            validator.resume_score(str(i))
+        elif(dirs[i-1].find("docx")>0):
+            print(dirs[i-1])
+            validator.resume_score1(str(i))
+        print(i)
 
-	f = open('./data/pstatus', 'w')
-	f.write(str(total))
-	f.close()
+    f = open('./data/pstatus', 'w')
+    f.write(str(total))
+    f.close()
 
-	return "preprocessing complete\n" + str(n) + " entries pushed to redis"
+    return "preprocessing complete\n" + str(n) + " entries pushed to redis"
 
 @app.route('/<i>', methods=['GET', 'POST'])
 def giveMeJSON(i):
-	curr_resume = "resume" + str(i)
-	result = json.loads(r.get(curr_resume).decode('utf8').replace("'", '"'))
-	return jsonify(result)
+    curr_resume = "resume" + str(i)
+    result = json.loads(r.get(curr_resume).decode('utf8').replace("'", '"'))
+    return jsonify(result)
 
 @app.route('/checker', methods=['GET', 'POST'])
 def checker():
 
-	if request.method == 'POST' and 'cgpa' in request.form and 'college' in request.form and 'skill' in request.form:
-		cgpa = request.form['cgpa']
-		college = request.form['college']
-		skill = request.form['skill']
+    if request.method == 'POST' and 'cgpa' in request.form and 'college' in request.form and 'skill' in request.form:
+        cgpa = request.form['cgpa']
+        college = request.form['college']
+        skill = request.form['skill']
 
-		f = open('./data/pstatus', 'r')
-		processed = f.read()
-		f.close()
-	
-		n = int(processed)
+        f = open('./data/pstatus', 'r')
+        processed = f.read()
+        f.close()
+    
+        n = int(processed)
 
-		all_resumes = {}
-		
-		for i in range(1, n + 1):
-			point = 0
-			curr_resume = "resume" + str(i)
-			try:
-				print(str(i) + " : ", end="")
-				payload = json.loads(r.get(curr_resume).decode('utf8').replace("'", '"'))
-				try:
-					if(float(payload["cgpa"] > cgpa)):
-						point += 1
-				except:
-					print("", end="")
-				try:
-					if(payload["college"] != "null"):
-						point += 1
-				except:
-					print("", end="")
-				try:
-					sFound = "N"
-					for skill_test in payload["skills"]:
-						if(skill in skill_test.lower()):
-							point += 1
-							sFound = "Y"
-							break
-				except:
-					print("", end="")
+        all_resumes = {}
+        
+        for i in range(1, n + 1):
+            point = 0
+            curr_resume = "resume" + str(i)
+            try:
+                print(str(i) + " : ", end="")
+                payload = json.loads(r.get(curr_resume).decode('utf8').replace("'", '"'))
+                try:
+                    if(float(payload["cgpa"] > cgpa)):
+                        point += 1
+                except:
+                    print("", end="")
+                try:
+                    if(payload["college"] != "null"):
+                        point += 1
+                except:
+                    print("", end="")
+                try:
+                    sFound = "N"
+                    for skill_test in payload["skills"]:
+                        if(skill in skill_test.lower()):
+                            point += 1
+                            sFound = "Y"
+                            break
+                except:
+                    print("", end="")
 
-				print(str(point) + "/3")
-				href = os.getcwd() + "/data/resume_dataset/resume" + str(i) + ".pdf"
-				all_resumes[str(i)] = {"Points" : point, "CGPA" : float(payload["cgpa"]), "College" : payload["college"], "Skill Found (Y/N)" : sFound, "Location" : href }
+                print(str(point) + "/3")
+                href = os.getcwd() + "/data/resume_dataset/resume" + str(i) + ".pdf"
+                all_resumes[str(i)] = {"Points" : point, "CGPA" : float(payload["cgpa"]), "College" : payload["college"], "Skill Found (Y/N)" : sFound, "Location" : href }
 
-			except:
-				print("null")
+            except:
+                print("null")
 
-		return jsonify(all_resumes)
+        return jsonify(all_resumes)
 
 
 if __name__ == '__main__':
-	app.secret_key = 'super secret key'
-	app.config['SESSION_TYPE'] = 'filesystem'
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
 
-	app.run(debug=True)
+    app.run(debug=True)
--- src/validator.py
+++ src/validator.py
@@ -1,4 +1,5 @@
 from src.extract import scrape_pdf
+from src.extract1 import scrape_docx
 from src.utils import get_cgpa, get_college, get_skills
 
 import spacy
@@ -15,49 +16,83 @@ from decimal import Decimal
 
 def find_college(college, n):
 
-	f = open("./data/college_dataset/T1", 'r')
-	college_dataset = f.read().lower().replace(',', '').splitlines()
+    f = open("./data/college_dataset/T1", 'r')
+    college_dataset = f.read().lower().replace(',', '').splitlines()
 
-	college = college.replace(',', '').replace('.', '').replace('–', '').replace('-', '').replace('(', '').replace(')', '')
+    college = college.replace(',', '').replace('.', '').replace('–', '').replace('-', '').replace('(', '').replace(')', '')
 
-	for single_college in college_dataset:
-		if(single_college in college):
-			return single_college
+    for single_college in college_dataset:
+        if(single_college in college):
+            return single_college
 
-	return "null"
+    return "null"
 
 # determine the resume score
 
 def resume_score(n):
 
-	filename = "./data/resume_dataset/resume" + n + ".pdf"
-	itemx = scrape_pdf(filename)
-	nlp = spacy.load('en_core_web_sm')
-	item = nlp(itemx)
+    filename = "./data/resume_dataset/resume" + n + ".pdf"
+    itemx = scrape_pdf(filename)
+    nlp = spacy.load('en_core_web_sm')
+    item = nlp(itemx)
 
-	skill_payload = sent_tokenize(itemx)
-	
-	resume_data = {}
-	
-	cgpa = get_cgpa(item)
-	resume_data["cgpa"] = cgpa
+    skill_payload = sent_tokenize(itemx)
+    
+    resume_data = {}
+    
+    cgpa = get_cgpa(item)
+    resume_data["cgpa"] = cgpa
 
-	skills = get_skills(item, n)
-	resume_data["skills"] = skills
+    skills = get_skills(item, n)
+    resume_data["skills"] = skills
 
-	def skill_calc():
-		for foo in skill_payload:
-			foo = foo.splitlines()
-			for inn in foo:
-				college = get_college(inn)
-	
-				if college is not None:
-					mycollege = find_college(college, n)
-					if(mycollege != "null"):
-						resume_data["college"] = mycollege
-						return True
-	if(not skill_calc()):
-		resume_data["college"] = "null"
+    def skill_calc():
+        for foo in skill_payload:
+            foo = foo.splitlines()
+            for inn in foo:
+                college = get_college(inn)
+    
+                if college is not None:
+                    mycollege = find_college(college, n)
+                    if(mycollege != "null"):
+                        resume_data["college"] = mycollege
+                        return True
+    if(not skill_calc()):
+        resume_data["college"] = "null"
 
-	resume_json = json.dumps(resume_data, ensure_ascii=False)
-	r.set("resume" + n, resume_json)
+    resume_json = json.dumps(resume_data, ensure_ascii=False)
+    r.set("resume" + n, resume_json)
+
+def resume_score1(n):
+    #print("rs1",n)
+    filename = "./data/resume_dataset/resume" + n + ".docx"
+    itemx = scrape_docx(filename)
+    nlp = spacy.load('en_core_web_sm')
+    item = nlp(itemx)
+    print("item")
+    skill_payload = sent_tokenize(itemx)
+    
+    resume_data = {}
+    
+    cgpa = get_cgpa(item)
+    resume_data["cgpa"] = cgpa
+
+    skills = get_skills(item, n)
+    resume_data["skills"] = skills
+
+    def skill_calc():
+        for foo in skill_payload:
+            foo = foo.splitlines()
+            for inn in foo:
+                college = get_college(inn)
+    
+                if college is not None:
+                    mycollege = find_college(college, n)
+                    if(mycollege != "null"):
+                        resume_data["college"] = mycollege
+                        return True
+    if(not skill_calc()):
+        resume_data["college"] = "null"
+
+    resume_json = json.dumps(resume_data, ensure_ascii=False)
+    r.set("resume" + n, resume_json) 
